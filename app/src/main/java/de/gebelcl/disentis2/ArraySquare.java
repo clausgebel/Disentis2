eac46af7924be5060753296f99abdff80be475d9
thekrazykat <60964813+thekrazykat@users.noreply.github.com>
2020-12-04 11:30:21 +0100

    Code simplified
    Remarks added and ArraySquare class enhanced.

--- app/src/main/java/de/gebelcl/disentis2/ArraySquare.java
+++ app/src/main/java/de/gebelcl/disentis2/ArraySquare.java
@@ -4,49 +4,73 @@ import android.util.Log;
 import android.view.View;
 
 public class ArraySquare {
-    public static void changeArraySquare(boolean[][] array, int x, int y) {
+    private boolean[][] imageStates;
+
+    public ArraySquare() {
+        imageStates = new boolean[][]{
+                {true, true, true, true, true},
+                {true, true, true, true, true},
+                {true, true, true, true, true},
+                {true, true, true, true, true},
+                {true, true, true, true, true}
+        };
+    }
+
+    public boolean[][] getImageStates() {
+        return imageStates;
+    }
+
+    public void setImageStates(boolean[][] imageStates) {
+        this.imageStates = imageStates;
+    }
+
+    public boolean getImageState(int x, int y) {
+        return imageStates[x][y];
+    }
+
+    public void changeArraySquare(int x, int y) {
         // First Line
          if (y > 0 && x == 0) {
-            array[x][y-1] = !array[x][y-1];
-            array[x+1][y-1] = !array[x+1][y-1];
+            imageStates[x][y-1] = !imageStates[x][y-1];
+             imageStates[x+1][y-1] = !imageStates[x+1][y-1];
         } else if (y > 0 && x == 4) {
-             array[x-1][y-1] = !array[x-1][y-1];
-             array[x][y-1] = !array[x][y-1];
+             imageStates[x-1][y-1] = !imageStates[x-1][y-1];
+             imageStates[x][y-1] = !imageStates[x][y-1];
          } else if (y > 0){
-             array[x-1][y-1] = !array[x-1][y-1];
-             array[x][y-1] = !array[x][y-1];
-             array[x+1][y-1] = !array[x+1][y-1];
+             imageStates[x-1][y-1] = !imageStates[x-1][y-1];
+             imageStates[x][y-1] = !imageStates[x][y-1];
+             imageStates[x+1][y-1] = !imageStates[x+1][y-1];
          }
 
         // Second Line
         if (x == 0) {
-            array[x+1][y] =  !array[x+1][y];
+            imageStates[x+1][y] =  !imageStates[x+1][y];
         } else if (x == 4) {
-            array[x-1][y] = !array[x-1][y];
+            imageStates[x-1][y] = !imageStates[x-1][y];
         } else {
-            array[x-1][y] = !array[x-1][y];
-            array[x+1][y] = !array[x+1][y];
+            imageStates[x-1][y] = !imageStates[x-1][y];
+            imageStates[x+1][y] = !imageStates[x+1][y];
         }
 
         //Third Line
         if (y < 4 && x == 0) {
-            array[x][y+1] = !array[x][y+1];
-            array[x+1][y+1] = !array[x+1][y+1];
+            imageStates[x][y+1] = !imageStates[x][y+1];
+            imageStates[x+1][y+1] = !imageStates[x+1][y+1];
         } else if (y < 4 && x == 4) {
-            array[x-1][y+1] = !array[x-1][y+1];
-            array[x][y+1] = !array[x][y+1];
+            imageStates[x-1][y+1] = !imageStates[x-1][y+1];
+            imageStates[x][y+1] = !imageStates[x][y+1];
         } else if (y < 4 && x < 4) {
-            array[x-1][y+1] = !array[x-1][y+1];
-            array[x][y+1] = !array[x][y+1];
-            array[x+1][y+1] = !array[x+1][y+1];
+            imageStates[x-1][y+1] = !imageStates[x-1][y+1];
+            imageStates[x][y+1] = !imageStates[x][y+1];
+            imageStates[x+1][y+1] = !imageStates[x+1][y+1];
         }
     }
 
-    public static int getXTag(View tag) {
+    public int getXTag(View tag) {
         return Integer.parseInt(tag.getTag().toString().substring(0,2));
     }
 
-    public static int getYTag(View tag) {
+    public int getYTag(View tag) {
         return Integer.parseInt(tag.getTag().toString().substring(2,4));
     }
 }
--- app/src/main/java/de/gebelcl/disentis2/MainActivity.java
+++ app/src/main/java/de/gebelcl/disentis2/MainActivity.java
@@ -3,67 +3,51 @@ package de.gebelcl.disentis2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayout;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
-
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
-    GridLayout gridLayout;
-    ImageView[][] images;
+    private TextView textView;
+    private GridLayout gridLayout;
+    private ImageView[][] images;
 
-    // true = tile is visible, false = tile is invisible
-    boolean[][] imageState = {
-            {true, true, true, true, true},
-            {true, true, true, true, true},
-            {true, true, true, true, true},
-            {true, true, true, true, true},
-            {true, true, true, true, true}
-    };
+    ArraySquare square = new ArraySquare();
 
     public void onImageViewClicked(View view) {
         ImageView imageView = (ImageView) view;
-        
 
-        textView.setText(view.getTag().toString());
-        textView.setText("[" + ArraySquare.getXTag(view) +"][" +ArraySquare.getYTag(view)+"]");
-        textView.setText("" +imageState[0][0]);
-
-        textView.setText("");
-        if (imageState[ArraySquare.getXTag(view)][ArraySquare.getYTag(view)]) {
-            ArraySquare.changeArraySquare(imageState, ArraySquare.getXTag(view), ArraySquare.getYTag(view));
-        }
-        textView.setText("" +imageState[0][1]);
-        for (int y = 0; y < 5; y++) {
-            for (int x = 0; x < 5; x++) {
-                 if (imageState[x][y])
-                    images[x][y].setAlpha(1.0f);
-                else
-                    images[x][y].setAlpha(0.0f);
-            }
-        }
-
-        boolean won = true;
-        for (boolean[] is: imageState) {
-            for (boolean is2: is) {
-                if (is2 == true) {
-                    won = false;
-                    break;
+        // if square is hidden with a tile then react, else do nothing
+        if (square.getImageState(square.getXTag(view), square.getYTag(view))) {
+            square.changeArraySquare(square.getXTag(view), square.getYTag(view));
+            for (int y = 0; y < 5; y++) {
+                for (int x = 0; x < 5; x++) {
+                    if (square.getImageState(x, y))
+                        images[x][y].setAlpha(1.0f);
+                    else
+                        images[x][y].setAlpha(0.0f);
                 }
             }
-            if (won == false)
-                break;
-        }
 
-        if (won == true) {
-            textView.setVisibility(View.VISIBLE);
-            textView.setText("Herzlichen Glückwunsch, Sie haben gewonnen!");
+            // if every tile is hidden, then show the text "Herzlichen Glückwunsch, Sie haben gewonnen!"
+            boolean won = true;
+            for (boolean[] is : square.getImageStates()) {
+                for (boolean is2 : is) {
+                    if (is2 == true) {
+                        won = false;
+                        break;
+                    }
+                }
+                if (won == false)
+                    break;
+            }
+
+            if (won == true) {
+                textView.setVisibility(View.VISIBLE);
+                textView.setText("Herzlichen Glückwunsch, Sie haben gewonnen!");
+            }
         }
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView);
         gridLayout = findViewById(R.id.gridLayout);
         images = new ImageView[5][5];
+
+        // Read all images from GridLayout
         int counter = 0;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
@@ -79,8 +65,5 @@ public class MainActivity extends AppCompatActivity {
                 counter++;
             }
         }
-
     }
-
-
 }
